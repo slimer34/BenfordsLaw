a1bd5633c80c9ad739ba9e8e13cf9d95411bc2d6
MurasakiKanmei <83978374+MurasakiKanmei@users.noreply.github.com>
2021-05-12 18:53:04 -0400

    Update BenfordLaw.java

--- BenfordLaw.java
+++ BenfordLaw.java
@@ -1,6 +1,6 @@
 /*
- * Date: 2021/11/10
- * Name: Daniel Yuan and Nikhil Sachdev
+ * Date: 2021/05/10
+ * Name: Daniel Yuan
  * Teacher: Mr. Ho
  * Description: Use Benford's Law to analyze a sales data file 
  * 1. Read the sales.csv file
@@ -8,46 +8,126 @@
  * 3. Export the digit frequency in a csv file with a table
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.FileNotFoundException;
 
+// TODO: make the percentage an Array with the percentages of 1-9 occuring in the data
+// TODO: write the file with percentages and tables
+
+
 class BenfordLaw {
-	public static void main(String [] args) throws FileNotFoundException {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws FileNotFoundException {
+
+		Scanner reader = new Scanner(System.in);
+		String readFile;
+		String[] salesNumbers = null;
+
+		System.out.println("Make sure your sales file is titled 'sales.csv'.");
+		System.out.println("Would you like to import the sales file? (yes/no)");
+
+		readFile = reader.nextLine();
+		if (readFile.equalsIgnoreCase("yes")) {
+			try {
+				salesNumbers = dataReader();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		else {
+			System.out.println("The program has been ended");
+		}
 		
+		if (benfordVerification(salesNumbers)) {
+			System.out.println("There is a possibility you did not commit fraud! Congratulations!");
+			generateSalesData();
+		} else {
+			System.out.println(
+					"Stop! You violated the law! Pay the court a fine or serve your sentence! \n Your stolen goods are now forfeit.");
+		}
+		
+	}
+
+	public static String[] dataReader() throws FileNotFoundException {
 		String fileName = "sales.csv"; // Read the sales.csv file
-		File salesFile = new File(fileName); 
-		
+		String line;
+		String currentSalesAmount;
+		int numLines = 0;
+		double[] frequencyArray = new double[9];
+		Arrays.fill(frequencyArray, 0);
+		String[] currentLineArray;
+		File salesFile = new File(fileName);
+
+		Scanner fileCounter = new Scanner(salesFile);
 		Scanner reader = new Scanner(salesFile);
-		String[] salesNumbers = fileName.split(","); // Reads everything from the sales.csv file after the comma delimiter
+
+		// Read file, take second part after comma, and put in array
+		// Get counter for every time a line is read, and use that number as the array
+		// address/amount (skip first line)
+		
+		
+		
+		while (fileCounter.hasNext()) {
+			numLines++;
+			fileCounter.next();
+		}
+
+		String salesNumberArray[] = new String[numLines-2];
+		
+		reader.next();
+		reader.next();
+		
+		for (int count = 0; count <= numLines-3; count++) {
+			line = reader.next();
+			currentLineArray = line.split("\\,"); // Reads everything from the sales.csv file after the comma delimiter
+			currentSalesAmount = currentLineArray[1];
+			salesNumberArray[count] = currentSalesAmount;
+		}
+
+		for (String sale: salesNumberArray) {
+			char firstDigitChar = sale.charAt(0); // save the first character of the string
+			int firstDigit = Character.getNumericValue(firstDigitChar); // reconvert to integer
+			frequencyArray[firstDigit - 1]++;
+			
+		}
 		
 		reader.close();
+		System.out.println(Arrays.toString(salesNumberArray));
+		System.out.println(Arrays.toString(frequencyArray));
+		return salesNumberArray;
+
 	}
-	
-	public static boolean benfordVerification(String salesNumbers) { //dataReader method has important stuff (including salesNumbers?)
-		int counter1 = 0
-		int precent = 0
+
+	public static boolean benfordVerification(String salesNumbers[]) {
+		double counter1 = 0;
+		double precent = 0;
 		for (int i = 0; i < salesNumbers.length; i++) {
-			char saleNumbers1 = (saleNumbers[i]).charAt(0);
+			String saleNumbers1 = Character.toString((salesNumbers[i]).charAt(0));
 			if (saleNumbers1.equals("1")) {
 				counter1++;
 			}
-			else {
-				continue;
-			}
 		}
-		precent = (counter1 / (salesNumbers.length)) * 100
+		precent = (counter1 / (salesNumbers.length)) * 100;
 		if (precent >= 29 && precent <= 32) {
 			return true;
-		}	
+			
+		} 
 		else {
 			return false;
 		}
 	}
-	
-	
-	public static void generateSalesData(String salesChart) {
-		File salesData = new File("results.csv");
-		
+
+	public static void generateSalesData() {
+		try{
+			File salesData = new File("results.csv");
+			FileWriter write = new FileWriter("results.csv");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
